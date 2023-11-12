2bd73aaa97d47e8c142af70e677c046be18fa259
zees007 <zees007@rediffmail.com>
2023-11-12 21:50:03 +0530

    initial commit

---
+++ src/main/java/com/openai/chatgtp/ChatgtpApplication.java
@@ -0,0 +1,15 @@
+package com.openai.chatgtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class ChatgtpApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgtpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openai/chatgtp/ChatgtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openai.chatgtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgtpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
